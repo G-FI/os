ee7e6c8414d7a2e903fdfe7151c034660b935ccd
hpy <657963486@qq.com>
2022-01-17 12:52:27 +0800

    cursor_port control

---
+++ drivers/port.c
@@ -0,0 +1,24 @@
+
+unsigned char port_byte_in(unsigned short port){
+    unsigned char result;
+    //in %ax, %dx; from %dx read data to ax
+    //the port number must be in dx
+    __asm__("in %%dx, %%al": "=a"(result):"d"(port));
+    return result;
+}
+
+void port_byte_out(unsigned short port, unsigned char data){
+    //Transfers byte in AL,word in AX or dword in EAX to the specified hardware port address
+    //out port, data
+    __asm__("out %%al, %%dx": : "a"(data), "d"(port));
+}
+
+unsigned short port_word_in(unsigned short port){
+    unsigned short result;
+    __asm__("in %%dx, %%ax": "=a"(result): "d"(port));
+    return result;
+}
+
+void port_wrod_out(unsigned short port, unsigned short data){
+    __asm__("out %%ax, %%dx":: "a"(data), "d"(port));
+}
---
+++ drivers/ports.h
@@ -0,0 +1,8 @@
+//read a byte from I/O port
+unsigned char port_byte_in(unsigned short port);
+//write a byte to I/O port
+void port_byte_out(unsigned short port, unsigned char data);
+//read a word 
+unsigned short port_word_in(unsigned short port);
+//write a word 
+void port_word_out(unsigned short port, unsigned short data);
